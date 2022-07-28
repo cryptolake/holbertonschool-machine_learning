39ba30d4c1bb79825e0e249020d93006f9258142
cryptolake <dhiadah@gmail.com>
2022-07-28 22:41:16 +0100

    task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Bracing The Elements."""
+
+
+def np_elementwise(mat1, mat2):
+    """Bracing The Elements."""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
