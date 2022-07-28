54548c4c851e7c5e4a81dac763265c119e65487b
cryptolake <dhiadah@gmail.com>
2022-07-28 21:18:27 +0100

    task 7

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Cat matrices."""
+
+
+def deep_copy(A):
+    """Deep copy."""
+    return [r.copy() for r in A]
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Cat matrices."""
+    mat = deep_copy(mat1)
+    if axis == 0:
+        for line in mat2:
+            mat.append(line)
+        return mat
+    elif axis == 1:
+        try:
+            for i, col in enumerate(mat2):
+                for ele in col:
+                    mat[i].append(ele)
+            return mat
+        except Exception:
+            return None
+    else:
+        return None
