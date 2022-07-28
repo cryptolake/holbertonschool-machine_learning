6e726abdcc1a5536c0011bf9cba7b8b454ad8b60
cryptolake <dhiadah@gmail.com>
2022-07-28 18:54:39 +0100

    task 5

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Matrix addition."""
+
+
+def shape(ele, shp):
+    """Recursive function to get shape."""
+    if not isinstance(ele, list):
+        return shp
+    shp.append(len(ele))
+    return shape(ele[0], shp)
+
+
+def add_matrices2D(mat1, mat2):
+    """Matrix addition."""
+    if mat1 == [] and mat2 == []:
+        return []
+    if shape(mat1, []) != shape(mat2, []):
+        return None
+    mat = []
+    for i in range(len(mat1)):
+        mat.append([])
+        for j in range(len(mat1[0])):
+            mat[i].append(mat1[i][j]+mat2[i][j])
+    return mat
