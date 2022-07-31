68ad056024ecb9da1d58a3c79c0bd1277fbebb0f
cryptolake <dhiadah@gmail.com>
2022-07-31 13:20:17 +0100

    fixes

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -2,6 +2,19 @@
 """Cat matrices."""
 
 
+def shape(ele, shp):
+    """Recursive function to get shape."""
+    if not isinstance(ele, list):
+        return shp
+    shp.append(len(ele))
+    return shape(ele[0], shp)
+
+
+def matrix_shape(matrix):
+    """Caller for the recur func."""
+    return shape(matrix, [])
+
+
 def deep_copy(A):
     """Deep copy."""
     return [r.copy() for r in A]
@@ -12,7 +25,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if mat1 is None or mat2 is None:
         return None
     mat = deep_copy(mat1)
+    shap1, shap2 = matrix_shape(mat1), matrix_shape(mat2)
     if axis == 0:
+        if shap1[1] != shap2[1]:
+            return None
         for line in mat2:
             mat.append(line)
         return mat
