6954148157dc4a1b44c4a18054ff87679e705dbc
cryptolake <dhiadah@gmail.com>
2022-07-31 18:22:27 +0100

    fixes

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -13,10 +13,14 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return None
     mat = deep_copy(mat1)
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         for line in mat2:
             mat.append(line)
         return mat
     elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         for i, col in zip(range(len(mat1)), mat2):
             if col is not None:
                 for ele in col:
