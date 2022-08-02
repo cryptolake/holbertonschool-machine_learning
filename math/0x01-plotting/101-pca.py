dd3e7398cad7649198546669c661a1298c032abe
cryptolake <dhiadah@gmail.com>
2022-08-02 20:59:18 +0100

    going 3d

---
+++ math/0x01-plotting/101-pca.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+plasma = plt.get_cmap("plasma")
+ax.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2], cmap=plasma)
+plt.show()
