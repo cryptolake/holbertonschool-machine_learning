cfd5b54d20588e8f487336cbc9926069002d6406
cryptolake <dhiadah@gmail.com>
2022-08-04 22:42:38 +0100

    integrate

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Poly integration."""
+
+
+def poly_integral(poly, C=0):
+    """Polynomial Integration."""
+    poly.insrt(0, C)
+    integ = []
+    for i, x in enumerate(poly):
+        integ.append(x)
+        if x != 0:
+            integ[i] = x / i
+    return integ
