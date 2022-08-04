7a8d2ba021c65226d512574279429d8cb24f703d
cryptolake <dhiadah@gmail.com>
2022-08-04 22:24:35 +0100

    testing

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -18,6 +18,6 @@ def summation_i_squared(n):
     If n is not a valid number, return None
     You are not allowed to use any loops
     """
-    if n < 1 or type(n) is not int:
+    if n < 1 or type(n) is not int or type(n) is not float:
         return None
-    return recur_sum(n, 0)
+    return recur_sum(int(n), 0)
