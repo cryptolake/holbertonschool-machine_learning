c82bdfd8686fb3fdf4f3fb03e5c38728222de8da
cryptolake <dhiadah@gmail.com>
2022-08-04 22:46:50 +0100

    integrate

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -10,4 +10,6 @@ def poly_integral(poly, C=0):
         integ.append(x)
         if x != 0:
             integ[i] = x / i
+            if integ[i] == int(integ[i]):
+                integ[i] = int(integ[i])
     return integ
