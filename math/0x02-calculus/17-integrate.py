04d9db2553326baf322274ed79ae3d399fde1152
cryptolake <dhiadah@gmail.com>
2022-08-05 03:03:17 +0100

    fixin

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,9 @@ def poly_integral(poly, C=0):
     """Polynomial Integration."""
     if type(poly) is not list or type(C) is not int or len(poly) == 0:
         return None
+    if len(poly) == 1 and poly[0] == 0:
+        poly[0] = C
+        return poly
     poly.insert(0, C)
     integ = []
     for i, x in enumerate(poly):
