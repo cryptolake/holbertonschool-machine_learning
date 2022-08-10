96942bfedbe82a212871c314f8f82c8897226dff
cryptolake <dhiadah@gmail.com>
2022-08-10 21:29:30 +0100

    TO THE MOON

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Exponential distribution.
+
+the exponential distribution is the probability
+distribution of the time between events
+in a Poisson point process, i.e., a process
+in which events occur continuously and
+independently at a constant average rate.
+"""
+
+
+class Exponential:
+    """Class of Exponential."""
+
+    def __init__(self, data=None, lambtha=1):
+        """Initialize."""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(sum(data) / len(data))
