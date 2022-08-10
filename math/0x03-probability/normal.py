77871ba313a3454f4f49eb4d35f9d1d733b8492c
cryptolake <dhiadah@gmail.com>
2022-08-10 23:36:05 +0100

    convert word to pdf

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -7,6 +7,16 @@ is a type of continuous probability
 distribution for a real-valued random variable.
 """
 
+PI = 3.1415926536
+E = 2.7182818285
+
+
+def erf(x):
+    """Error function provided."""
+    return 2/(PI ** (1/2)) * (
+           x - (x ** 3) / 3 + (x ** 5) / 10 - (x ** 7) / 42 +
+           (x ** 9) / 216)
+
 
 class Normal:
     """Class of Normal."""
@@ -34,3 +44,8 @@ class Normal:
     def x_value(self, z):
         """Get x from z score."""
         return (self.stddev * z) + self.mean
+
+    def pdf(self, x):
+        """Probability density function."""
+        return 1/((self.stddev*(2*PI)**(1/2)) **
+                  -1/2*((x-self.mean)/self.stddev))
