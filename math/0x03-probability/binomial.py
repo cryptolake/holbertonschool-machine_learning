5eab0f81d64690542dbebf139e213acb2fd32c64
cryptolake <dhiadah@gmail.com>
2022-08-11 21:29:11 +0100

    bipolar combination

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Binomial distribution.
+
+ the binomial distribution with parameters n and p
+ is the discrete probability distribution of
+ the number of successes in a sequence of n
+ independent experiments, each asking a yes–no question.
+"""
+
+
+def round(x):
+    """Get round."""
+    n = {int(x+1): int(x+1)-x, int(x): x - int(x)}
+    return min(n, key=n.get)
+
+
+def mean(data):
+    """Get mean."""
+    return sum(data) / len(data)
+
+
+def variance(data):
+    """Get variance."""
+    mn = mean(data)
+    return (sum([(x - mn) ** 2 for x in data])
+            / len(data))
+
+
+class Binomial:
+    """Class of Binomial."""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """Initialize."""
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            if p > 1 or p <= 0:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.p = abs((variance(data) / mean(data)) - 1)
+            self.n = round(mean(data) / self.p)
