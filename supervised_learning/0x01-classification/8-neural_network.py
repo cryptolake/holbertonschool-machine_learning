032daf2d97d36ef71362b0d15faf0409b696b2d4
cryptolake <dhiadah@gmail.com>
2022-08-23 19:49:10 +0100

    the begining of the network

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""from one neuron to a network."""
+import numpy as np
+
+
+class NeuralNetwork:
+    """A Neural Network from a neuron."""
+
+    def __init__(self, nx, nodes):
+        """
+        Initialize The Neural Network.
+
+        nx: the number of input features to the neuron.
+        nodes: the number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((3, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
