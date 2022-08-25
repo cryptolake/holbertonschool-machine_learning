d9e0e9fa23d2d3b6c46ad50907d4e8ba4d6f5a24
cryptolake <dhiadah@gmail.com>
2022-08-25 20:36:10 +0100

    tensors flowing in my blood

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tensorflow."""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """Create placeholders."""
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return x, y
