41f422b25cf2f20237e2c4836b6ccaba3aca6563
cryptolake <dhiadah@gmail.com>
2022-08-25 21:58:34 +0100

    create tensorflow layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -6,7 +6,7 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """Create tensorflow layer."""
+    weight = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(n, activation=activation, name="layer",
-                                  kernel_initializer=tf.keras.\
-                                  initializers.VarianceScaling(mode='fan_avg'))
+                                  kernel_initializer=weight)
     return layer(prev)
