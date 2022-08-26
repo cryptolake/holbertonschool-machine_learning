ddd552c8bbf207cdc4a5f20529ccfa483d5b46c2
cryptolake <dhiadah@gmail.com>
2022-08-26 19:41:31 +0100

    getting trained well

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -47,30 +47,31 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
 
     saver = tf.train.Saver()
     sess = tf.Session()
-    sess.run(init)
-    for i in range(iterations):
-        t_loss, t_accur = sess.run((loss, accuracy), feed_dict={
-            x: X_train, y: Y_train})
-        v_loss, v_accur = sess.run((loss, accuracy), feed_dict={
-            x: X_valid, y: Y_valid})
-        if i % 100 == 0:
-            print(f"""After {i} iterations:
-\tTraining Cost: {t_loss}
-\tTraining Accuracy: {t_accur}
-\tValidation Cost: {v_loss}
-\tValidation Accuracy: {v_accur}""")
-        _ = sess.run(train_op, feed_dict={
-            x: X_train, y: Y_train})
+    with tf.Sesstion() as sess:
+        sess.run(init)
+        for i in range(iterations):
+            t_loss, t_accur = sess.run((loss, accuracy), feed_dict={
+                x: X_train, y: Y_train})
+            v_loss, v_accur = sess.run((loss, accuracy), feed_dict={
+                x: X_valid, y: Y_valid})
+            if i % 100 == 0:
+                print(f"""After {i} iterations:
+    \tTraining Cost: {t_loss}
+    \tTraining Accuracy: {t_accur}
+    \tValidation Cost: {v_loss}
+    \tValidation Accuracy: {v_accur}""")
+            _ = sess.run(train_op, feed_dict={
+                x: X_train, y: Y_train})
 
-    i += 1
-    if i % 100 == 0:
-        t_loss, t_accur = sess.run((loss, accuracy), feed_dict={
-            x: X_train, y: Y_train})
-        v_loss, v_accur = sess.run((loss, accuracy), feed_dict={
-            x: X_valid, y: Y_valid})
-        print(f"""After {i} iterations:
-\tTraining Cost: {t_loss}
-\tTraining Accuracy: {t_accur}
-\tValidation Cost: {v_loss}
-\tValidation Accuracy: {v_accur}""")
-    return saver.save(sess, save_path, iterations)
+        i += 1
+        if i % 100 == 0 and iterations % 100 == 0:
+            t_loss, t_accur = sess.run((loss, accuracy), feed_dict={
+                x: X_train, y: Y_train})
+            v_loss, v_accur = sess.run((loss, accuracy), feed_dict={
+                x: X_valid, y: Y_valid})
+            print(f"""After {i} iterations:
+    \tTraining Cost: {t_loss}
+    \tTraining Accuracy: {t_accur}
+    \tValidation Cost: {v_loss}
+    \tValidation Accuracy: {v_accur}""")
+        return saver.save(sess, save_path, iterations)
