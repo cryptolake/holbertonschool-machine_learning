493ef025303270cb60dd571f63bcd9ed5accbb2e
cryptolake <dhiadah@gmail.com>
2022-08-26 11:54:46 +0100

    calculate accuracy

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""tensorflow accuracy."""
+
+import tensorflow.compat.v1 as tf
+
+def calculate_accuracy(y, y_pred):
+    """Accuracy of prediction."""
+    correct = tf.where(tf.math.equal(y, y_pred),y, y_pred)
+
+    return tf.reduce_mean(correct)
