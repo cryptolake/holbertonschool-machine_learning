bc4e36d086c82355323e8dcde0472f455afcf5af
cryptolake <dhiadah@gmail.com>
2022-08-26 13:40:39 +0100

    training operation

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Gradient descent in tensorflow."""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """Create training operation."""
+    train = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+    grads = train.compute_gradients(loss)
+    return train.apply_gradients(grads)
+
+
