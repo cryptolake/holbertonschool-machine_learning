2620eac5b39df4bf17e1e65cf435a1fb28974a1f
cryptolake <dhiadah@gmail.com>
2022-08-27 22:12:58 +0100

    getting trained well

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -41,12 +41,12 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
 
     saver = tf.train.Saver()
 
-    tf.add_to_collection(x, 'x')
-    tf.add_to_collection(y, 'y')
-    tf.add_to_collection(y_pred, 'y_pred')
-    tf.add_to_collection(loss, 'loss')
-    tf.add_to_collection(accuracy, 'accuracy')
-    tf.add_to_collection(train_op, 'train_op')
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
 
     with tf.Session() as sess:
         sess.run(init)
