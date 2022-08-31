b8167ea1fbe31246e6a5d057d94ac73ea7a8075d
cryptolake <dhiadah@gmail.com>
2022-08-31 20:38:04 +0100

    one hot encoding

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -10,12 +10,6 @@ def one_hot_encode(Y, classes):
         return None
     if classes < 2:
         return None
-    if np.min(Y) < 0:
-        return None
-    if np.max(Y) > classes:
-        return None
-    if type(Y) is not np.ndarray:
-        return None
     y_one_hot = np.zeros((Y.size, classes))
     y_one_hot[Y, np.arange(classes)] = 1
     return y_one_hot
