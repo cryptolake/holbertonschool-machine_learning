db7dedd28ee1bdc20b41f7cf79644986f6d2f227
cryptolake <dhiadah@gmail.com>
2022-08-31 20:39:12 +0100

    testing

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,10 +6,16 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """One hot encoding on classes."""
-    if type(classes) is not int:
-        return None
-    if classes < 2:
-        return None
+    # if type(classes) is not int:
+    #     return None
+    # if classes < 2:
+    #     return None
+    # if np.min(Y) < 0:
+    #     return None
+    # if np.max(Y) < classes:
+    #     return None
+    # if type(Y) is not np.ndarray:
+    #     return None
     y_one_hot = np.zeros((Y.size, classes))
     y_one_hot[Y, np.arange(classes)] = 1
     return y_one_hot
