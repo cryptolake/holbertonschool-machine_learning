09f13fd446d80805cece1a35889db305fb829237
cryptolake <dhiadah@gmail.com>
2022-09-03 19:04:16 +0100

    finally understood tensorflow

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -9,7 +9,11 @@ def evaluate(X, Y, save_path):
     saver = tf.train.import_meta_graph(save_path+'.meta')
     with tf.Session() as sess:
         saver.restore(sess, tf.train.latest_checkpoint('./'))
-        y_pred = tf.get_collection('y_pred')
-        accuracy = tf.get_collection('accuracy')
-        cost = tf.get_collection('loss')
-        return y_pred[0], accuracy[0], cost[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        pred, t_loss, t_accur = sess.run((y_pred, loss, accuracy),
+                                         feed_dict={x: X, y: Y})
+        return pred, t_accur, t_loss
