b11a1f58910056dcc9f20c7243d1e2444f56a721
cryptolake <dhiadah@gmail.com>
2022-09-06 16:37:24 +0100

    RMSProp in tensorflow

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RMSProp optimization algorithm."""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Implement RMSProp with tensorflow."""
+    train = tf.train.RMSPropOptimizer(alpha, epsilon=epsilon, momentum=beta2)
+    grads = train.compute_gradients(loss)
+    return train.apply_gradients(grads)
