1b87900682a11360e2cba34315b6f249d23a9963
cryptolake <dhiadah@gmail.com>
2022-09-07 20:47:49 +0100

    tensorflow

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -16,11 +16,13 @@ def create_batch_norm_layer(prev, n, activation):
 
     Returns: a tensor of the activated output for the layer
     """
-    mean = tf.math.reduce_mean(prev)
-    var = tf.math.reduce_variance(prev)
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(n, kernel_initializer=init)
+    z = layer(prev)
     gamma = tf.Variable(1., trainable=True)
     beta = tf.Variable(0., trainable=True)
-    normalized = tf.nn.batch_normalization(prev, mean, var, beta, gamma, 1e-8)
-    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(n, activation, kernel_initializer=init)
-    return layer(normalized)
+    mean = tf.math.reduce_mean(z, axis=0)
+    var = tf.math.reduce_variance(z, axis=0)
+    epsilon = 1e-8
+    normalized = tf.nn.batch_normalization(z, mean, var, beta, gamma, epsilon)
+    return activation(normalized)
