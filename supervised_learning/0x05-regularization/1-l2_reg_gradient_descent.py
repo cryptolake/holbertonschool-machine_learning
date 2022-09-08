456d4b2f071a0a56095bd52c016a4c82ba41cddc
cryptolake <dhiadah@gmail.com>
2022-09-08 21:00:55 +0100

    L2 reg

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""L2 Regulazation in Gradient descent."""
+import numpy as np
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """L2 Regulazation in Gradient descent."""
+    m = len(Y[0])
+    dz = cache['A'+str(L)] - Y
+    for i in range(L, 0, -1):
+        dw = (1/m) * np.matmul(dz, cache['A'+str(i-1)].T)\
+            + (lambtha / m)*weights['W'+str(i)]
+        db = (1/m) * np.sum(dz, axis=1, keepdims=True)
+        da = np.matmul(weights['W'+str(i)].T, dz)
+        dz = da * (1-cache['A'+str(i-1)]**2)
+
+        weights['W'+str(i)] = weights['W'+str(i)]\
+            - alpha * dw
+        weights['b'+str(i)] = weights['b'+str(i)]\
+            - alpha * db
