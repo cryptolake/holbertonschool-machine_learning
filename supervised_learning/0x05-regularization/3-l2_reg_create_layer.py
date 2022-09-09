3bc46a895d5efbde7a8c5e711db53e8381387f75
cryptolake <dhiadah@gmail.com>
2022-09-09 13:14:08 +0100

    tensorflow layer for L2

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -6,6 +6,9 @@ import tensorflow.compat.v1 as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Create layer with L2 Regulazation."""
     l2 = tf.keras.regularizers.L2(lambtha)
+    weight = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                   mode=("fan_avg"))
     layer = tf.layers.Dense(n, activation=activation,
-                                  kernel_regularizer=l2)
+                            kernel_initializer=weight,
+                            kernel_regularizer=l2)
     return layer(prev)
