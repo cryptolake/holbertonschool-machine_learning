bfc61a716c97e7ffbebe08fb8e17ad96b479f8d9
cryptolake <dhiadah@gmail.com>
2022-09-09 11:30:47 +0100

    L2 in tf

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -5,5 +5,5 @@ import tensorflow.compat.v1 as tf
 
 def l2_reg_cost(cost):
     """L2 regulazation cost in tensorflow."""
-    reg = tf.keras.regularizers.L2()
-    return reg(cost)
+    reg = tf.losses.get_regularization_losses()
+    return cost + reg
