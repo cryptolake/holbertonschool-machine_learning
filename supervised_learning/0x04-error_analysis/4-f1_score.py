64b7150231c9183221533cc205532b6f3192378d
cryptolake <dhiadah@gmail.com>
2022-09-11 23:23:22 +0100

    f1 score

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Get F1 score from confusion matrix."""
+import numpy as np
+
+
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """Get F1 score from confusion matrix."""
+    pres = precision(confusion)
+    sens = sensitivity(confusion)
+    return 2 *((pres * sens)/(pres + sens))
