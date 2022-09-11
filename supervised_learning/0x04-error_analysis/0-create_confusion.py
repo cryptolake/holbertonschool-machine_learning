bd5c44f37a5327d29c2d775a8e48206a9ae4c706
cryptolake <dhiadah@gmail.com>
2022-09-11 21:16:44 +0100

    confusion matrix try

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python3
+"""Confusion Matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """Create Confusion Matrix."""
+    cm = np.zeros((labels.shape[1], labels.shape[1]))
+    m = labels.shape[0]
+    for i in range(m):
+        inda = np.argwhere(labels[i] == 1)
+        indo = np.argwhere(logits[i] == 1)
+        cm[inda, indo] += 1 
+    return cm
+
