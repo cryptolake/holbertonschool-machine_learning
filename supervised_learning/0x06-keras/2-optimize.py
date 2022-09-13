7c5534df21fd7b7a2666ccaeca8c1e3aa83a01f7
cryptolake <dhiadah@gmail.com>
2022-09-13 21:45:04 +0100

    Optimize model

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Optimize and compile Keras model."""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Set up Adam optimization for a keras model with\
+    categorical crossentropy loss and accuracy metrics.
+
+    network is the model to optimize
+    alpha is the learning rate
+    beta1 is the first Adam optimization parameter
+    beta2 is the second Adam optimization parameter
+    Returns: None
+    """
+    optimizer = K.optimizers.Adam(alpha, beta1, beta2)
+    network.compile(optimizer=optimizer,
+                    loss=K.losses.CategoricalCrossentropy(),
+                    metrics=['accuracy'])
+    return None
