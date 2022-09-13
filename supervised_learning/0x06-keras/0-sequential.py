20f8191bccfaea2a6083c107d1313ffcf53e70df
cryptolake <dhiadah@gmail.com>
2022-09-13 19:50:06 +0100

    Keras model

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -37,5 +37,5 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                                      activation=activations[i],
                                      kernel_regularizer=l2))
         if i != len(layers)-1:
-            model.add(K.layers.Dropout(keep_prob))
+            model.add(K.layers.Dropout(1-keep_prob))
     return model
---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Build Keras model."""
+import tensoflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Build a neural network with the Keras library.
+
+    nx is the number of input features to the network
+
+    layers is a list containing the number of nodes in
+    each layer of the network
+
+    activations is a list containing the activation functions
+    used for each layer of the network
+
+    lambtha is the L2 regularization parameter
+
+    keep_prob is the probability that a node will be kept for dropout
+    Returns: the keras model
+    """
+    prev = K.Input(shape=(nx,))
+    inputs = prev
+    l2 = K.regularizers.L2(lambtha)
+    for i, layer in enumerate(layers):
+        prev = K.layers.Dense(layer, activation=activations[i],
+                              kernel_regularizer=l2)(prev)
+        if i != len(layers) - 1:
+            prev = K.layers.Dropout(1-keep_prob)(prev)
+
+    model = K.Model(inputs=inputs, outputs=prev)
+    return model
