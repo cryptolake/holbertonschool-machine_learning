1e1dbf7d215e17c83769d52d1770e37882801de0
cryptolake <dhiadah@gmail.com>
2022-09-13 21:50:52 +0100

    Optimize model

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -17,5 +17,5 @@ def optimize_model(network, alpha, beta1, beta2):
     optimizer = K.optimizers.Adam(alpha, beta1, beta2)
     network.compile(optimizer=optimizer,
                     loss=K.losses.CategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy'])
+                    metrics='accuracy')
     return None
