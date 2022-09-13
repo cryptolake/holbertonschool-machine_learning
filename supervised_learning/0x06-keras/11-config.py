a126f61f4c9cff9c3aedc1460e13fc4a26819bb9
cryptolake <dhiadah@gmail.com>
2022-09-13 23:54:01 +0100

    save and load config

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Save and restore keras Model weights."""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """Save model."""
+    with open(filename, 'w') as modeljson:
+        modeljson.write(network.to_json())
+    return None
+
+
+def load_configg(filename):
+    """Load model."""
+    with open(filename, 'r') as modeljson:
+        model = K.models.model_from_json(modeljson.read())
+    return model
