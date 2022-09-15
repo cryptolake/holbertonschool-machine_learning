a434d3c8bacf8223ad7894ffaa73f50ca45ea7e0
cryptolake <dhiadah@gmail.com>
2022-09-15 20:53:50 +0100

    fixes

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -20,11 +20,11 @@ def convolve_grayscale_same(images, kernel):
     """
     kh, kw = kernel.shape
     _, h, w = images.shape
+    output = np.zeros(images.shape)
     ph = int(np.ceil((kh-1)/2))
     pw = int(np.ceil((kw-1)/2))
     npad = ((0, 0), (ph, ph), (pw, pw))
     images = np.pad(images, pad_width=npad, mode='constant')
-    output = np.zeros(images.shape)
     for i in range(h):
         for j in range(w):
             output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel,
