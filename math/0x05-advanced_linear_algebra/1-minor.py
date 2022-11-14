0e0c5f9a13a65d5ee75cea756ed681f426afb815
cryptolake <dhiadah@gmail.com>
2022-11-14 16:59:25 +0100

    find matrix of minors: better way

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,11 +2,37 @@
 """Find matrix of minors."""
 
 
-def cross_prod(matrix):
-    """Get cross product."""
+def determinant(matrix):
+    """Find Determinant."""
+    for x in matrix:
+        if type(x) is not list:
+            raise TypeError('matrix must be a list of lists')
+    if len(matrix) == 1:
+        if len(matrix[0]) == 0:
+            return 1
+        else:
+            return matrix[0][0]
+    lm = len(matrix[0])
+    for s in matrix:
+        if len(s) != lm or len(s) != len(matrix):
+            raise ValueError('matrix must be a square matrix')
+        lm = len(s)
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    return matrix[0][0]
+    else:
+        b = 0
+        c = 1
+        mat = matrix.copy()
+        mat.pop(0)
+        for i in range(len(matrix)):
+            matt = mat.copy()
+            for j in range(len(matt)):
+                matt[j] = mat[j].copy()
+            for x in matt:
+                x.pop(i)
+            b += c * matrix[0][i] * determinant(matt)
+            c *= -1
+        return b
 
 
 def minor(matrix):
@@ -14,8 +40,10 @@ def minor(matrix):
     for x in matrix:
         if type(x) is not list:
             raise TypeError('matrix must be a list of lists')
+    if len(matrix) == 0:
+        raise TypeError('matrix must be a list of lists')
     if len(matrix) == 1:
-        return 1
+        return [[1]]
     lm = len(matrix[0])
     for s in matrix:
         if len(s) != lm or len(s) != len(matrix):
@@ -32,5 +60,5 @@ def minor(matrix):
                 matt[j] = mat[j].copy()
             for m in matt:
                 m.pop(y)
-            matmin[x].append(cross_prod(matt))
+            matmin[x].append(determinant(matt))
     return matmin
