5cc951c8011920bda29f0f9ffe78e41cea8acb97
cryptolake <dhiadah@gmail.com>
2022-11-14 15:46:06 +0100

    after all of that no imports...

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
-import numpy as np
-
-
-def find_minor(index, matrix):
-    """Find minor for matrix."""
-    mins = []
-    for i in range(len(index)):
-        a = list(range(matrix.shape[i]))
-        a.remove(index[i])
-        mins.append(a)
-    return matrix[np.ix_(*mins)]
 
 
 def determinant(matrix):
     """Find Determinant."""
-    matrix = np.array(matrix)
-    if len(matrix.shape) == 1:
-        raise TypeError('matrix must be a list of lists')
-    if matrix.shape == (1, 0):
-        return 1
-    if matrix.shape == (1, 1):
-        return matrix[0, 0]
-    nl = matrix.shape[0]
-    for s in matrix.shape[1:]:
-        if s != nl:
+    for x in matrix:
+        if type(x) is not list:
+            raise TypeError('matrix must be a list of lists')
+    if len(matrix) == 1:
+        if len(matrix[0]) == 0:
+            return 1
+        else:
+            return matrix[0][0]
+    lm = len(matrix[0])
+    for s in matrix:
+        if len(s) != lm:
             raise ValueError('matrix must be a square matrix')
-    if matrix.shape == (2, 2):
-        return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     else:
         b = 0
         c = 1
-        for i in range(matrix.shape[1]):
-            b += c * matrix[0, i] * determinant(find_minor([0, i], matrix))
+        mat = matrix.copy()
+        mat.pop(0)
+        for i in range(len(matrix)):
+            matt = mat.copy()
+            for j in range(len(matt)):
+                matt[j] = mat[j].copy()
+            for x in matt:
+                x.pop(i)
+            b += c * matrix[0][i] * determinant(matt)
             c *= -1
         return b
