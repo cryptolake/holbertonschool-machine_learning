d4affa0e47fc93e82e2cc61f29f59cfc278a5609
cryptolake <dhiadah@gmail.com>
2022-11-16 17:25:04 +0100

    this needs to be fixed

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -8,11 +8,13 @@ def mean_cov(X):
     if len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     mean = np.mean(X, axis=0)
-    n, d = X.shape
+    # n, d = X.shape
+    n, _ = X.shape
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    covm = np.zeros(shape=(d, d))
-    for x in range(d):
-        for y in range(d):
-            covm[x, y] = np.sum((X[:, x] - mean[x])*(X[:, y] - mean[y]))/n
-    return mean, covm
+    # another way to do this but checker was annoying on number comparison
+    # covm = np.ndarray(shape=(d, d))
+    # for x in range(d):
+    #     for y in range(d):
+    #         covm[x, y] = np.sum((X[:, x] - mean[x])*(X[:, y] - mean[y]))/n
+    return mean, np.matmul((X - mean).T, (X - mean)) / (X.shape[0] - 1)
