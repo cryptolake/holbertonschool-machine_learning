f5c6074f893e0355e4f1699a87a74c9f4416f6b5
cryptolake <dhiadah@gmail.com>
2022-11-17 13:13:34 +0100

    correlation matrix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -6,9 +6,9 @@ import numpy as np
 def correlation(C):
     """Get correlation matrix."""
     if type(C) is not np.ndarray:
-        raise TypeError
-    if len(C.shape) != 2:
-        raise ValueError
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
     D = np.sqrt(np.diag(C))
     dinv = np.linalg.inv(np.diag(D))
     corr = dinv @ C @ dinv
