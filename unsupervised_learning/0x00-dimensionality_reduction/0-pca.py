41ef9d4cb998467a0bd3e84caa7bea208bd20b09
cryptolake <dhiadah@gmail.com>
2022-11-28 16:14:59 +0100

    use svd

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -5,12 +5,7 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Perform pca while keeping features that cover at least var variance."""
-    X = X / X.std()
-    cov = X.T @ X
-    eig_val, eig_vec = np.linalg.eig(cov)
-    oi = np.argsort(eig_val)[::-1]
-    sorted_eigenvectors = eig_vec[:, oi]
-    explained = [i / sum(eig_val) for i in eig_val]
-    explained = np.cumsum(explained)
-    arg = np.argwhere(explained >= var)[0, 0]
-    return -1*sorted_eigenvectors[:, 0:arg]
+    _, s, v = np.linalg.svd(X)
+    cum_var = np.cumsum(s) / np.sum(s)
+    arg = np.argwhere(cum_var >= var)[0, 0]
+    return v[:arg+1].T
