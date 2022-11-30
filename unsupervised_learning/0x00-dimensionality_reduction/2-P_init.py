ea5dc4b1b86a4379b971bee8b2e968ec75a4bf75
cryptolake <dhiadah@gmail.com>
2022-11-30 15:41:43 +0100

    initialize p for t-sne

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -8,6 +8,7 @@ def P_init(X, perplexity):
     # Math? See https://stackoverflow.com/questions/37009647
     sum_X = np.sum(np.square(X), 1)
     D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    np.fill_diagonal(D, 0)
     P = np.zeros((X.shape[0], X.shape[0]))
     betas = np.ones((X.shape[0], 1))
     H = np.log2(perplexity)
