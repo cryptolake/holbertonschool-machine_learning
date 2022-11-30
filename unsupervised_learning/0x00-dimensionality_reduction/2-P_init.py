075a262aa259f3e849bab883d8fe839464478257
cryptolake <dhiadah@gmail.com>
2022-11-30 15:37:45 +0100

    initialize p for t-sne

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""t-SNE the new dimensionality reduction method."""
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """Initialize P in t-SNE."""
+    # Math? See https://stackoverflow.com/questions/37009647
+    sum_X = np.sum(np.square(X), 1)
+    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
+    P = np.zeros((X.shape[0], X.shape[0]))
+    betas = np.ones((X.shape[0], 1))
+    H = np.log2(perplexity)
+    return D, P, betas, H
