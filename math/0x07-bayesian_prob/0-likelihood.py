eb5dafba1aecddb865b8d0cbea7028ec2bd5ccdb
cryptolake <dhiadah@gmail.com>
2022-12-08 13:50:52 +0100

    bayesian

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -11,9 +11,9 @@ def comb(n, k):
 
 def likelihood(x, n, p):
     """Likelihood."""
-    if n <= 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if x < 0:
+    if type(x) is not int or x < 0:
         raise ValueError("x must be an integer that\
                 is greater than or equal to 0")
     if x > n:
