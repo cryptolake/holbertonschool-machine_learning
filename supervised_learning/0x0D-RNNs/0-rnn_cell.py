a64013071ed008bee3ef1e6d03365c2e31b912a9
cryptolake <dhiadah@gmail.com>
2022-12-12 15:21:31 +0100

    RNN cell done

---
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Initialize RNN cell."""
+import numpy as np
+
+
+class RNNCell:
+    """RNN Cell."""
+
+    def __init__(self, i, h, o):
+        """Initialize class."""
+        self.Wh = np.random.normal(size=(i, h))
+        self.bh = np.zeros(shape=(1, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.by = np.zeros(shape=(1, o))
+
+    @staticmethod
+    def tanh(x):
+        """Sigmoid activation."""
+        return (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
+
+    @staticmethod
+    def softmax(x):
+        """Softmax activation."""
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+    def forward(self, h_prev, x_t):
+        """Perform forward propagation."""
+        h_t = self.tanh(x_t @ self.Wh + h_prev + self.bh)
+        y_t = self.softmax(h_t @ self.Wy + self.by)
+        return h_t, y_t
