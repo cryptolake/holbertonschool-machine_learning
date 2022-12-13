040d375a15dda3580b93c7189d3cf2f2b2aa8ee6
cryptolake <dhiadah@gmail.com>
2022-12-13 12:50:07 +0100

    fixes

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -8,24 +8,31 @@ class RNNCell:
 
     def __init__(self, i, h, o):
         """Initialize class."""
-        self.Wh = np.random.normal(size=(i, h))
+        self.Wh = np.random.normal(size=(i+h, h))
         self.bh = np.zeros(shape=(1, h))
         self.Wy = np.random.normal(size=(h, o))
         self.by = np.zeros(shape=(1, o))
 
     @staticmethod
-    def tanh(x):
-        """Sigmoid activation."""
-        return (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
+    def sigmoid(z):
+        """Sigmoid function."""
+        return 1.0/(1.0+np.exp(-z))
 
     @staticmethod
-    def softmax(x):
-        """Softmax activation."""
-        return np.exp(x) / np.sum(np.exp(x), axis=0)
+    def tanh(z):
+        """Tanh activation function."""
+        return ((np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z)))
+
+    @staticmethod
+    def softmax(z):
+        """Softmax function."""
+        ex = np.exp(z)
+        return ex / np.sum(ex, axis=1, keepdims=True)
 
     def forward(self, h_prev, x_t):
         """Perform forward propagation."""
         # Current state
-        c_t = self.tanh(x_t @ self.Wh + h_prev + self.bh)
+        z = np.concatenate((h_prev, x_t), axis=1) @ self.Wh + self.bh
+        c_t = self.tanh(z)
         y_t = self.softmax(c_t @ self.Wy + self.by)
         return c_t, y_t
