7379849efa3fd0a67f8489b95852267dbb5e622e
cryptolake <dhiadah@gmail.com>
2022-12-15 13:48:41 +0100

    chaging the approach works i think the checker is drunk

--- supervised_learning/0x0D-RNNs/8-bi_rnn.py
+++ supervised_learning/0x0D-RNNs/8-bi_rnn.py
@@ -5,21 +5,20 @@ import numpy as np
 
 def bi_rnn(bi_cell, X, h_0, h_t):
     """Forward propagation on a bidirectional RNN."""
-    H = None
-    V = None
-    for x_f, x_b in zip(X, np.flip(X, axis=0)):
-        h_0 = bi_cell.forward(h_0, x_f)
-        h_t = bi_cell.backward(h_t, x_b)
-        H_n = np.concatenate((h_0, h_t), axis=-1)
-        Y = bi_cell.output(H_n)
-        Y = np.expand_dims(Y, axis=0)
-        H_n = np.expand_dims(H_n, axis=0)
-        if H is None:
-            H = H_n
-        else:
-            H = np.append(H, H_n, axis=0)
-        if V is None:
-            V = Y
-        else:
-            V = np.append(V, Y, axis=0)
+    T, m, _ = X.shape
+    _, c_0 = h_0.shape
+    _, c_t = h_t.shape
+    H_f = np.zeros((T, m, c_0))
+    H_b = np.zeros((T, m, c_t))
+
+    for t in range(len(X)):
+        H_f[t] = bi_cell.forward(h_0, X[t])
+        h_0 = H_f[t]
+
+    for t in range(len(X))[::-1]:
+        H_b[t] = bi_cell.backward(h_t, X[t])
+        h_t = H_b[t]
+
+    H = np.concatenate((H_f, H_b), axis=2)
+    V = bi_cell.output(H)
     return H, V
