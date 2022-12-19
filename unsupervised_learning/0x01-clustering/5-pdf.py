da6421ee2f241727263389dd47087c92ce5a99c0
cryptolake <dhiadah@gmail.com>
2022-12-19 19:40:54 +0100

    fixed: it needs to be matrix operations

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -5,10 +5,19 @@ import numpy as np
 
 def pdf(X, m, S):
     """PDF of the gaussian distribution (multivariate)."""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+
     _, d = X.shape
+
+    if type(m) is not np.ndarray or len(m.shape) != 1 or m.shape[0] != d:
+        return None
+    if type(S) is not np.ndarray or len(S.shape) != 2 or S.shape != (d, d):
+        return None
+
     x_m = X - m
     det = np.linalg.det(S)
     inv = np.linalg.inv(S)
     num = np.exp(-0.5 * np.sum(x_m * np.matmul(inv, x_m.T).T, axis=1))
     denum = np.sqrt(((2*np.pi)**d)*det)
-    return num/denum
+    return np.maximum(num/denum, 1e-300)
