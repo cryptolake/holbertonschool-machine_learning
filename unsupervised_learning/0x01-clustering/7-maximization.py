dba8441c471c03fd705872ec2b0c892104d73ffc
cryptolake <dhiadah@gmail.com>
2022-12-20 18:41:58 +0100

    maximization

---
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Maximization step in EM algorithm for GMM."""
+import numpy as np
+
+
+def maximization(X, g):
+    """Perform Maximization step in EM algorithm for GMM."""
+    n, _ = X.shape
+    k, _ = g.shape
+    pi = []
+    m = []
+    S = []
+    for j in range(k):
+        yj = np.expand_dims(g[j], axis=1)
+        pi.append(yj.sum()/n)
+        m.append(np.expand_dims(np.sum(yj * X, axis=0),
+                                axis=0)/np.sum(yj))
+        diff = (X - m[j])
+        weighted_sum = (g[j] * diff.T) @ diff
+        S.append(np.expand_dims(weighted_sum / np.sum(yj), axis=0))
+    m = np.concatenate(m, axis=0)
+    pi = np.array(pi)
+    S = np.concatenate(S, axis=0)
+    return pi, m, S
