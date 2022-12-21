c6fff617540e41cfd685e1f444bb322937a13d51
cryptolake <dhiadah@gmail.com>
2022-12-21 17:58:57 +0100

    Perform Bayesian Information Criterion to determine numbers of clusters

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 """Perform BIC for best cluster GMM."""
 import numpy as np
-
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """Perform BIC for best cluster GMM."""
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
 
-    if type(iterations) is not int or iterations <= 0:
+    if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
 
-    if type(tol) is not float:
+    if not isinstance(kmax, int) or kmax < 1:
         return None, None, None, None
 
-    if type(verbose) is not bool:
+    if kmax - kmin < 1:
         return None, None, None, None
 
-    if type(kmin) is not int or kmin < 1:
-        return None, None, None, None
+    if not isinstance(iterations, int) or iterations < 1:
+        return None, None, None, None, None
 
-    if kmax is None:
-        kmax = X.shape[0]
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
 
-    if type(kmax) is not int or kmax < 1:
-        return None, None, None, None
-
-    if kmax - kmin < 2:
-        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
 
     BIC = []
     res = []
@@ -38,12 +34,12 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     n, d = X.shape
 
     for k in k_s:
-        pi, m, S, _, ll = expectation_maximization(X, k, iterations,
-                                                   tol, verbose=verbose)
-        logl.append(ll)
+        pi, m, S, _, L = expectation_maximization(X, k, iterations,
+                                                  tol, verbose)
+        logl.append(L)
         res.append((pi, m, S))
         p = k*d + k*(d*(d+1)/2) + k-1
-        bic = p * np.log(n) - 2 * ll
+        bic = p * np.log(n) - 2 * L
         BIC.append(bic)
     best = np.argmin(BIC)
     return k_s[best], res[best], np.array(logl), np.array(BIC)
