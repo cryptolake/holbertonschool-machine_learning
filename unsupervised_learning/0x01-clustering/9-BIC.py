cc2c96fb01aa3ac1fa5f31bb4e712a53751cc649
cryptolake <dhiadah@gmail.com>
2022-12-21 16:44:26 +0100

    Perform Bayesian Information Criterion to determine numbers of clusters

---
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Perform BIC for best cluster GMM."""
+import numpy as np
+
+expectation_maximization = __import__('8-EM').expectation_maximization
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Perform BIC for best cluster GMM."""
+    if type(kmin) is not int or kmin < 1:
+        return None, None, None, None
+
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if type(kmax) is not int or kmax < 1:
+        return None, None, None, None
+
+    BIC = []
+    res = []
+    logl = []
+    k_s = list(range(kmin, kmax+1))
+    n, d = X.shape
+
+    for k in k_s:
+        pi, m, S, _, ll = expectation_maximization(X, k, iterations,
+                                                   tol, verbose)
+        if pi is None:
+            return None, None, None, None
+        logl.append(ll)
+        res.append((pi, m, S))
+        p = k*d + k*(d*(d+1)/2) + k-1
+        bic = p * np.log(n) - 2 * ll
+        BIC.append(bic)
+    best = np.argmin(BIC)
+    return k_s[best], res[best], np.array(logl), np.array(BIC)
