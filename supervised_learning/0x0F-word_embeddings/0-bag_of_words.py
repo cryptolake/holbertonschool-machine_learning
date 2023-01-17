9ce439bb8077aa2fbcf2385bc37b2c7006d4dc41
cryptolake <dhiadah@gmail.com>
2023-01-17 23:37:49 +0100

    Turns out i didn't need to code my own solution and i could just use sklearn... 😔😢😞😩😫😭😥😣

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 """Bag of words embedding."""
-import numpy as np
-
-
-def clean_string(string):
-    """Clean strings for processing."""
-    lower_str = string.lower().replace("'s", '')
-    proc_sentence = ''.join([c for c in lower_str if c.isalpha() or c == ' '])
-    return proc_sentence
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
@@ -17,12 +10,7 @@ def bag_of_words(sentences, vocab=None):
     sentences: the sentences to perform bow on
     vocab: the vocabulary to use if none use all words in sentences
     """
-    if vocab is None:
-        vocab = ' '.join(sentences)
-        vocab = list(np.unique(clean_string(vocab).split(' ')))
-    embeddings = np.zeros(shape=(len(sentences), len(vocab)))
-    for i, sentence in enumerate(sentences):
-        words = clean_string(sentence).split(' ')
-        for v, word in enumerate(vocab):
-            embeddings[i, v] = words.count(word)
-    return embeddings, vocab
+    cont_vector = CountVectorizer(vocabulary=vocab)
+    embeddings = cont_vector.fit_transform(sentences)
+    features = cont_vector.get_feature_names()
+    return embeddings.toarray(), features
