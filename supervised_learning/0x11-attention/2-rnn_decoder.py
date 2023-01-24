e2cf566d53ab0be140bd1fbbd293a1cd834186a0
cryptolake <dhiadah@gmail.com>
2023-01-24 17:00:26 +0100

    rnn decoder

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -5,36 +5,7 @@ SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """
-    Decoder layer for Machine translation.
-
-    Class constructor def __init__(self, vocab, embedding, units, batch):
-        vocab is an integer representing the size of the output vocabulary
-        embedding is an integer representing the dimensionality
-        of the embedding vector units is an integer representing the number
-        of hidden units in the RNN cell
-        batch is an integer representing the batch size
-        Sets the following public instance attributes:
-            embedding - a keras Embedding layer that converts
-            words from the vocabulary into an embedding vector
-            gru - a keras GRU layer with units units
-                Should return both the full sequence of outputs
-                as well as the last hidden state
-                Recurrent weights should be initialized with glorot_uniform
-            F - a Dense layer with vocab units
-    Public instance method def call(self, x, s_prev, hidden_states):
-        x is a tensor of shape (batch, 1) containing the previous word
-        in the target sequence as an index of the target vocabulary
-        s_prev is a tensor of shape (batch, units) containing the previous
-        decoder hidden state
-        hidden_states is a tensor of shape
-        (batch, input_seq_len, units)containing the outputs of the encoder
-        You should concatenate the context vector with x in that order
-        Returns: y, s
-            y is a tensor of shape (batch, vocab) containing the output
-            word as a one hot vector in the target vocabulary
-            s is a tensor of shape (batch, units) containing the new decoder hidden state
-    """
+    """Decoder layer for Machine translation."""
 
     def __init__(self, vocab, embedding, units, batch):
         """Initialize Class."""
@@ -56,9 +27,8 @@ class RNNDecoder(tf.keras.layers.Layer):
         x = tf.cast(x, dtype=tf.float64)
         context = tf.cast(context, dtype=tf.float64)
         context = tf.expand_dims(context, 1)
-        cont_x = tf.concat([context, x], axis=2)
-        y, hidden = self.gru(cont_x)
-        y = y[:, 0, :]
-        Y = self.F(y)
-
+        X = tf.concat([context, x], axis=-1)
+        y, hidden = self.gru(inputs=X)
+        Y = tf.reshape(y, (-1, y.shape[2]))
+        Y = self.F(Y)
         return Y, hidden
