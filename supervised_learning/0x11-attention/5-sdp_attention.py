f26ea0ab76cc0190b7e2117c746dfe653c60a1f2
cryptolake <dhiadah@gmail.com>
2023-01-28 01:29:58 +0100

    updated to optimized better version

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -6,22 +6,14 @@ import tensorflow as tf
 def sdp_attention(Q, K, V, mask=None):
     """Attention as in the 'attention is all you need' paper."""
     batches, _, dk = K.shape
-    weights = []
-    for batch in range(batches):
-        res = tf.expand_dims(Q[batch] @ tf.transpose(K[batch]), 0)
-        weights.append(res)
-
-    scaled = tf.concat(weights, 0)/tf.sqrt(tf.cast(dk, tf.float32))
+    K = tf.einsum('ijk->ikj', K)
+    scaled = tf.matmul(Q, K)/tf.sqrt(tf.cast(dk, tf.float32))
     if mask is not None:
         mask *= 1e9
         scaled += mask
     else:
         mask = 0
     att_weights = tf.nn.softmax(scaled)
+    sdp_att = tf.matmul(att_weights, V)
 
-    att = []
-    for batch in range(batches):
-        res = tf.expand_dims(att_weights[batch] @ V[batch], 0)
-        att.append(res)
-    sdp_att = tf.concat(att, 0)
     return sdp_att, att_weights
