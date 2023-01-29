d46d8c5c1c9a6bd776cff35a71b072405398c647
cryptolake <dhiadah@gmail.com>
2023-01-29 02:22:48 +0100

    testing..

--- supervised_learning/0x11-attention/6-multihead_attention.py
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -22,15 +22,18 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         """Functionality of the layer."""
         multi_head = []
         att_ws = []
-        for _ in range(self.h):
-            Q_l = self.Wq(Q)
-            K_l = self.Wk(K)
-            V_l = self.Wv(V)
+        Q = self.Wq(Q)
+        K = self.Wk(K)
+        V = self.Wv(V)
+        for i in range(0, self.dm, self.depth):
+            Q_l = Q[:, :, i:i+self.depth]
+            K_l = K[:, :, i:i+self.depth]
+            V_l = V[:, :, i:i+self.depth]
             sdp_a, att_w = sdp_attention(Q_l, K_l, V_l, mask)
+            att_w = tf.expand_dims(att_w, axis=1)
             multi_head.append(sdp_a)
             att_ws.append(att_w)
         multi_head = tf.concat(multi_head, axis=2)
-        att_ws = tf.stack(att_ws, axis=3)
-        att_ws = tf.einsum('ijkl->iljk', att_ws)
+        att_ws = tf.concat(att_ws, axis=1)
         Y = self.linear(multi_head)
         return Y, att_ws
