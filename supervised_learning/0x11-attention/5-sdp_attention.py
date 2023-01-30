21d0161627a146fe2b15cc742132599e3d4b799d
cryptolake <dhiadah@gmail.com>
2023-01-30 15:42:56 +0100

    still trying, it's litterally the same code but checker saying no, i'm probably wrong

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -5,12 +5,13 @@ import tensorflow as tf
 
 def sdp_attention(Q, K, V, mask=None):
     """Attention as in the 'attention is all you need' paper."""
-    dk = K.shape[-1]
-    scaled = tf.matmul(Q, K, transpose_b=True)/tf.sqrt(tf.cast(dk, tf.float32))
+    qk_matmul = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(K.shape[-1], tf.float32)
+    scaled = qk_matmul / tf.math.sqrt(dk)
     if mask is not None:
-        mask *= -1e9
-        scaled += mask
-    att_weights = tf.nn.softmax(scaled)
+        scaled += (mask * -1e9)
+
+    att_weights = tf.nn.softmax(scaled, axis=-1)
     sdp_att = tf.matmul(att_weights, V)
 
     return sdp_att, att_weights
