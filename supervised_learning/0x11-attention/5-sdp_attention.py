591aa6b42a042b4cb16790971a24984bd961763e
cryptolake <dhiadah@gmail.com>
2023-01-30 15:28:02 +0100

    still testing

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -8,10 +8,7 @@ def sdp_attention(Q, K, V, mask=None):
     dk = K.shape[-1]
     scaled = tf.matmul(Q, K, transpose_b=True)/tf.sqrt(tf.cast(dk, tf.float32))
     if mask is not None:
-        mask *= 1e9
         scaled += mask
-    else:
-        mask = 0
     att_weights = tf.nn.softmax(scaled)
     sdp_att = tf.matmul(att_weights, V)
 
