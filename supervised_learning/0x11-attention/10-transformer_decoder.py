d04423768c61fce59fbf1c7bb70f57bce9e799ab
cryptolake <dhiadah@gmail.com>
2023-02-02 02:25:28 +0100

    transformer decoder stack

--- supervised_learning/0x11-attention/10-transformer_decoder.py
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""Transformer encoder stack."""
+"""Transformer decoder stack."""
 import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
-EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
 
 
-class Encoder(tf.keras.layers.Layer):
+class Decoder(tf.keras.layers.Layer):
     """Transformer encoder stack."""
 
-    def __init__(self, N, dm, h, hidden, input_vocab,
-                 max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden,
+                 target_vocab, max_seq_len, drop_rate=0.1):
         """Initialize class."""
         super().__init__()
         self.N = N
         self.dm = dm
 
-        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
 
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
                        for _ in range(N)]
 
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
-    def call(self, x, training, mask):
+    def call(self, x, encoder_output, training, look_ahead_mask,
+             padding_mask):
         """Call the layer."""
         input_len = x.shape[1]
         p_e = self.positional_encoding[:input_len]
@@ -37,5 +38,6 @@ class Encoder(tf.keras.layers.Layer):
         x = self.dropout(x, training=training)
 
         for block in self.blocks:
-            x = block(x, training, mask)
+            x = block(x, encoder_output, training, look_ahead_mask,
+                      padding_mask)
         return x
