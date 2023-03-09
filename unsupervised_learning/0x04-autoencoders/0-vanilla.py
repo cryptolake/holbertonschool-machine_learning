d4684fe7fdbaef6f8757775e3f88f5ced9da98d7
cryptolake <dhiadah@gmail.com>
2023-03-09 11:32:58 +0100

    checker will only accept functional style keras :(

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -11,25 +11,49 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     Build an autoencoder.
     Args:
         input_dims: input dimension which is the same as the output
-        hidden_layers: number of nodes in each hidden layer
+        hidden_layers: list number of nodes in each hidden layer
         latent_dims: dimension of the latent space layer
     Return:
         Model: the autoencoder keras model
     """
-    encoder = keras.Sequential(
-        [keras.layers.Input((None, input_dims))] +
-        [keras.layers.Dense(x, activation='relu') for x in hidden_layers] +
-        [keras.layers.Dense(latent_dims)]
-    )
-    decoder = keras.Sequential(
-        [keras.layers.Input((None, latent_dims))] +
-        [keras.layers.Dense(x, activation='relu')
-         for x in reversed(hidden_layers)] +
-        [keras.layers.Dense(input_dims, activation='sigmoid')])
-    auto = keras.Sequential([
-        keras.layers.Input((None, 784)),
-        encoder,
-        decoder
-    ])
-    auto.compile(optimizer='adam', loss='binary_cross_entropy')
+    # encoder = keras.Sequential(
+    #     [keras.Input((None, input_dims))] +
+    #     [keras.layers.Dense(x, activation='relu') for x in hidden_layers] +
+    #     [keras.layers.Dense(latent_dims)]
+    # )
+    # decoder = keras.Sequential(
+    #     [keras.layers.Input((None, latent_dims))] +
+    #     [keras.layers.Dense(x, activation='relu')
+    #      for x in reversed(hidden_layers)] +
+    #     [keras.layers.Dense(input_dims, activation='sigmoid')])
+    # auto = keras.Sequential([
+    #     keras.Input((None, input_dims)),
+    #     encoder,
+    #     decoder
+    # ])
+
+    # Checker won't accept a keras Sequential model
+
+    # The encoder
+    X = keras.Input((input_dims))
+    en = X
+    for nl in hidden_layers:
+        en = keras.layers.Dense(nl, activation='relu')(en)
+    en_final = keras.layers.Dense(latent_dims, activation='relu')(en)
+    encoder = keras.Model(X, en_final)
+
+    # The Decoder
+    de_X = keras.Input((latent_dims))
+    de = de_X
+    for nl in hidden_layers[::-1]:
+        de = keras.layers.Dense(nl, activation='relu')(de)
+    de_final = keras.layers.Dense(input_dims, activation='sigmoid')(de)
+    decoder = keras.Model(de_X, de_final)
+
+    # The AutoEncoder
+    enc = encoder(X)
+    dec = decoder(enc)
+    auto = keras.Model(X, dec)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
     return encoder, decoder, auto
