ad0ecc672751efaaab63bc5876249bbb83a7fa4d
cryptolake <dhiadah@gmail.com>
2023-05-17 16:34:55 +0100

    pycodestyle

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -5,12 +5,17 @@ import requests
 
 def availableShips(passengerCount):
     """Return the list of ships that hold given number of passengers."""
-    starships_request = requests.get('https://swapi-api.hbtn.io/api/starships/').json()
+    starships_request = requests\
+        .get('https://swapi-api.hbtn.io/api/starships/')\
+        .json()
     starships = starships_request['results'].copy()
     next = starships_request['next']
     while next is not None:
         starships_request = requests.get(next).json()
         starships.extend(starships_request['results'].copy())
         next = starships_request.get('next')
-    available_starships = filter(lambda x: x['passengers'].replace(',', '').isdigit() and int(x['passengers'].replace(',', '')) >= passengerCount, starships)
+    available_starships = filter(lambda x: x['passengers'].replace(',', '')
+                                 .isdigit() and int(x['passengers']
+                                                    .replace(',', ''))
+                                 >= passengerCount, starships)
     return list(map(lambda x: x['name'], available_starships))
