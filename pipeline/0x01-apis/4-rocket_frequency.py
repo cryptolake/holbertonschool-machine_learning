886cbf415327b0fe79f73efbb0786d90d28da7ad
cryptolake <dhiadah@gmail.com>
2023-05-23 14:09:45 +0100

    rocket freq

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -2,12 +2,13 @@
 """Get number of rockets."""
 import requests
 
+
 def rocket_freq():
     """Get Rocket frequency."""
     rockets = requests.get('https://api.spacexdata.com/latest/rockets/').json()
     rockets = sorted(rockets, key=lambda x: x['name'])
-    rocket_dict = {rocket['id']: [rocket['name'], 0]\
-        for rocket in rockets}
+    rocket_dict = {rocket['id']: [rocket['name'], 0]
+                   for rocket in rockets}
 
     launches = requests.get('https://api.spacexdata.com/latest/launches')\
         .json()
@@ -15,10 +16,12 @@ def rocket_freq():
     for launch in launches:
         rocket_dict[launch['rocket']][1] += 1
 
-    rocket_dict = dict(sorted(rocket_dict.items(), key=lambda x: x[1][1], reverse=True))
+    rocket_dict = dict(sorted(rocket_dict.items(),
+                              key=lambda x: x[1][1], reverse=True))
 
     for rocket in rocket_dict.values():
         print("{}: {}".format(rocket[0], rocket[1]))
+
 
 if __name__ == "__main__":
     rocket_freq()
