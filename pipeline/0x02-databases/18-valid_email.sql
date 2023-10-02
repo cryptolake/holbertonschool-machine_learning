2dc1011013e7bb61cd0d7104c37613085bfa8667
cryptolake <dhiadah@gmail.com>
2023-10-02 21:00:35 +0100

    More sql triggers

---
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -0,0 +1,11 @@
+-- MORE TRIGGGGGGERS
+DELIMITER //
+
+CREATE TRIGGER onchange BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+  IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+  END IF;
+END //
+DELIMITER ;
