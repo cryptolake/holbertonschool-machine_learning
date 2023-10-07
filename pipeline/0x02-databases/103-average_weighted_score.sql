323734f4a1132d8e3f9628336bfb5321b8041628
cryptolake <dhiadah@gmail.com>
2023-10-07 21:10:56 +0100

    fix dummy

--- pipeline/0x02-databases/103-average_weighted_score.sql
+++ pipeline/0x02-databases/103-average_weighted_score.sql
@@ -1,8 +1,14 @@
 -- Average weighted score 
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-  SELECT AVG(corrections.score) FROM users INNER JOIN corrections ON users.id = corrections.user_id WHERE users.id = user_id GROUP BY users.id INTO @user_avg;
+  SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+  FROM users
+  INNER JOIN corrections ON users.id = corrections.user_id
+  INNER JOIN projects ON projects.id = corrections.project_id
+  WHERE
+  users.id = user_id
+  GROUP BY users.id INTO @user_avg;
   UPDATE users SET average_score = @user_avg WHERE id = user_id;
 END //
 DELIMITER ;
