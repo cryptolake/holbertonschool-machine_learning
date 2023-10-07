1d27bc909823f7b13f6445e70f860c83e7b11d49
cryptolake <dhiadah@gmail.com>
2023-10-07 14:34:18 +0100

    pymongo find within array

---
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Mongodb in python."""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Advanced find."""
+    return mongo_collection.find({"topics": {"$in": topic}})
